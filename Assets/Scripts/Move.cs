7ab1455fc466ea59bc8d24f573ac277e99f821b0
regireed89 <regireed89@gmail.com>
2017-05-05 10:03:22 -0500

    0

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("w"))
+        {
+            transform.position += transform.up;
+        }
+        if(Input.GetKey("a"))
+        {
+            transform.position += transform.right;
+        }
+	}
+}
